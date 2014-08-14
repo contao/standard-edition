ce773a3d17634f1c13edf33320813735b9b9e772
Leo Feyer <github@contao.org>
2014-08-14 20:08:49 +0200

    Use the Symfony class cache

--- system/initialize.php
+++ system/initialize.php
@@ -10,8 +10,9 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
-// Boot the Symfony kernel
 require_once __DIR__ . '/../app/bootstrap.php.cache';
 require_once __DIR__ . '/../app/AppKernel.php';
+
 $kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
 $kernel->boot();
