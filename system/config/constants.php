f8bfff3f0ed28b66449441804e0752f569ca64ba
Leo Feyer <github@contao.org>
2014-08-21 10:07:52 +0200

    Load DropZone via `contao-components`

--- system/config/constants.php
+++ system/config/constants.php
@@ -16,8 +16,6 @@ define('BUILD', '0-alpha2');
 define('LONG_TERM_SUPPORT', false);
 
 // Plugin versions (backwards compatibility)
-define('ACE', '1.1.3');
-define('DROPZONE', '3.8.5');
 define('HIGHLIGHTER', '3.0.83');
 define('HTML5SHIV', '3.7.0');
 define('JQUERY', '1.11.0');
