2009726167d6a45dcd4cac47383ea52b8fa333a3
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-04-27 16:30:36 +0200

    Improved security of password storage

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,29 +13,33 @@ use Symfony\Component\Debug\Debug;
 
 // Access to debug front controllers must only allowed from localhost or with username and password.
 // FIXME: add link to documentation that explains what to do.
-$md5Username = '';
-$md5Password = '';
+$accessKey = '';
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
 ) {
-    if ('' === $md5Username || '' === $md5Password) {
+    list($username, $password) = explode(':', $accessKey);
+
+    if ('' === $username || '' === $password) {
         header('HTTP/1.0 403 Forbidden');
         exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 
     } elseif (!isset($_SERVER['PHP_AUTH_USER'])
         || !isset($_SERVER['PHP_AUTH_PW'])
-        || md5($_SERVER['PHP_AUTH_USER']) !== $md5Username
-        || md5($_SERVER['PHP_AUTH_PW']) !== $md5Password
+        || hash('sha512', $_SERVER['PHP_AUTH_USER']) !== $username
+        || hash('sha512', $_SERVER['PHP_AUTH_PW']) !== $password
     ) {
         header('WWW-Authenticate: Basic realm="Contao Debug"');
         header('HTTP/1.0 401 Unauthorized');
         exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
     }
+
+    unset($username, $password, $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
 }
 
-unset($md5Username, $md5Password, $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+unset($accessKey);
+
 
 $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 Debug::enable();
