64f998a855de98657b9d92f506f605c0d1d3b859
Leo Feyer <github@contao.org>
2015-08-14 14:59:27 +0200

    Include the constants.php file in the install tool (see contao/installation-bundle#1).

--- web/install.php
+++ web/install.php
@@ -18,6 +18,7 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../app/AppKernel.php';
+require_once __DIR__ . '/../vendor/contao/core-bundle/src/Resources/contao/config/constants.php';
 require_once __DIR__ . '/../vendor/contao/core-bundle/src/Resources/contao/helper/functions.php';
 
 $kernel = new AppKernel('prod', false);
