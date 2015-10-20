b68a251d2e242c16657c5d7cfb16cc503350ad8a
Leo Feyer <github@contao.org>
2015-10-20 15:20:21 +0200

    Adjust the default configuration to the reworked install tool.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,6 +40,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Contao\InstallationBundle\ContaoInstallationBundle();
         }
 
         return $bundles;
